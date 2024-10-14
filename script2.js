8ba4fb03bb7f9f29efbf9dc62c9b6f3fd1f8f081
Samuel do nascimento ramos <hmsrp.ramos7@gmail.com>
2024-10-14 16:50:57 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,4 @@
+var base = parseInt(prompt("digite a base do retangulo"))
+var altura = parseInt(prompt("digite a altura do retangulo"))
+var area = base * altura
+alert ("a area do retangulo é:" + area)
